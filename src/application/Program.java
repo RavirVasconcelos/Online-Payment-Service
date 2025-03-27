dbfddbff2e4c3afaac40f31b2165da596d7eabf3
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2025-03-26 22:15:11 -0300

    Init Commit

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.nextLine(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+        sc.nextLine();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(obj, n);
+
+        System.out.println("PARCELAS");
+
+        for(Installment installment : obj.getInstallment()) {
+            System.out.println(installment);
+        }
+    }
+}
